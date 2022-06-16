5aeb1ccd75eb2ef37281aa1a9bc7c72618d7a4e4
zdliu2021 <1475879051@qq.com>
2022-06-16 14:00:28 +0800

    first commit

---
+++ prophet/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("upload", views.upload),
+    path("index", views.index),
+    path("getAllModels", views.getAllModels),
+    path("getAllDatasets", views.getAllDatasets),
+    path("getResultOfModel", views.getResultOfModel),
+]
