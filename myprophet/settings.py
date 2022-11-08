0bba8f79381e9d4bc211bb40b7171d7b69faf822
zdliu2021 <1475879051@qq.com>
2022-11-08 10:38:09 +0800

    WIP

--- myprophet/settings.py
+++ myprophet/settings.py
@@ -152,3 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, "static/"), # 首选project静态文件搜寻路径
+ )
